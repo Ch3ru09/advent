0a9c427ac782f3387e9500db730ca39fc8d10d1f
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-12-11 12:25:16 -0500

    day 3 baby

--- day3/pt2.py
+++ day3/pt2.py
@@ -5,22 +5,24 @@ b, c, g = 0, 0, 0
 
 
 def eh(e):
-    return int(e)*-1
+  return int(e)
 
 
 for d in range(len(f[0])):
-    f.sort(key=eh)
-    for x in f:
-        if x[d] == '0':
-            b += 1
-        else:
-            g += 1
-    if b >= g:
-        f = f[b-1::-1]
+  f.sort(key=eh)
+  for x in f:
+    if x[d] == '1':
+      b += 1
     else:
-        f = f[g-1::1]
-    c = 0
-    b = g = 0
+      g += 1
+  if b < g:
+    f = f[g::1]
+  else:
+    f = f[g-1::-1]
+  f.sort(key=eh)
+  if d == 0:
+    print(f)
+  c = 0
+  b = g = 0
 print(f)
-'101111110011'
-'000000000111'
+print(int('101011011101', 2) * int('001110010111', 2))
