cda2351416a68b172d6f9910c5d2b96fc5914d13
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-12-23 11:55:03 -0500

    idk

--- day4/pt1.py
+++ day4/pt1.py
@@ -29,23 +29,41 @@ def check(list1, val):
     return True
 
 
+ans = ''
+winNum = 0
 for x in ref:
     # getting every value in ref
     if p != 0:
         break
-    for e in boards:
+    for i, e in enumerate(boards):
         for el in e:
             if x in el:
-                el[el.index(x)] = -1
+                el[el.index(x)] += '-yes'
         temp = 0
+        temp2 = 0
         for column in range(len(e[0])):
             for row in e:
-                if row.count(-1) == len(row):
-                    print(e)
+                for n in row:
+                    if n.count('yes') > 0:
+                        temp2 += 1
+                if temp2 == len(row) and p == 0:
+                    ans = e
+                    winNum = x
                     p += 1
-                elif row[column] == -1:
+                temp2 = 0
+                if row[column] == -1:
                     temp += 1
             if temp == len(e):
-                print(e)
+                ans = e
+                winNum = x
                 p += 1
-                temp = 0
+            temp = 0
+print(ans, winNum)
+
+
+oof = 0
+for x in ans:
+    for e in x:
+        if e.count('yes') == 0:
+            oof += int(e)
+print(oof*int(winNum))
---
+++ day4/pt2.py
@@ -0,0 +1,60 @@
+f = open('data.txt', 'r').read().split('\n')
+
+ref = []
+boards = []
+buff = []
+i = 0
+for x in f:
+    if i < 1:
+        ref.append(f[0])
+        i += 1
+    else:
+        buff.append([e for e in x.split(' ') if e])
+        if x == '':
+            buff.pop()
+            boards.append(buff)
+            buff = []
+boards.pop(0)
+ref = ref[0].split(',')
+
+p = 0
+
+a = 0
+ans = []
+winNum = 0
+for x in ref:
+    # getting every value in ref:
+    for i, e in enumerate(boards):
+        if ans.count(e) < 1:
+            for el in e:
+                if x in el:
+                    el[el.index(x)] += '-yes'
+            temp = 0
+            temp2 = 0
+            for column in range(len(e[0])):
+                for row in e:
+                    for n in row:
+                        if n.count('yes') > 0:
+                            temp2 += 1
+                    if temp2 == len(row):
+                        if ans.count(e) < 1:
+                            ans.append(e)
+                    temp2 = 0
+                    if row[column] == -1:
+                        temp += 1
+                if temp == len(e):
+                    if ans.count(e) < 1:
+                        ans.append(e)
+                temp = 0
+    if len(boards) == len(ans):
+        winNum = int(x)
+        a = ans[len(ans)-1]
+
+print(winNum)
+
+oof = 0
+for x in ans[len(ans)-1]:
+    for e in x:
+        if e.count('yes') == 0:
+            oof += int(e)
+print(oof)
