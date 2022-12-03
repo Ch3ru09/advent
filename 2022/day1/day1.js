c1adf57814e7bd465ee5c24a93b7097f1839f132
Ch3ru09 <xliang0905@gmail.com>
2022-12-02 19:55:57 -0500

    done day 1

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -2,16 +2,36 @@ const fs = require("fs/promises");
 
 fs.readFile("./data.txt", { encoding: 'utf8' })
   .then(data => {
-    console.log(getMaxSum(data.split("\n")));
+    // console.log(getMaxSum(data.split("\r\n")));
+    console.log(getMax3(data.split("\r\n")));
   })
 
 function getMaxSum(data) {
-  let max = Number.NEGATIVE_INFINITY;
-  let temp = 0;
+  let largestNumber = Number.NEGATIVE_INFINITY;
+  let temporarySum = 0;
   data.forEach(calories => {
     if (calories == "") {
-      max = temp;
-      temp = 0;
+      largestNumber = Math.max(largestNumber, temporarySum);
+      temporarySum = 0;
+      return
     }
+    temporarySum += Number(calories);
   });
+  return largestNumber
+}
+
+function getMax3(data) {
+  let largestNumbers = new Array(3).fill(Number.NEGATIVE_INFINITY)
+  let temporarySum = 0;
+  data.forEach(calories => {
+    if (calories == "") {
+      largestNumbers.push(temporarySum)
+      largestNumbers.sort((a, b) => b - a)
+      largestNumbers.pop()
+      temporarySum = 0
+      return
+    }
+    temporarySum += Number(calories);
+  });
+  return largestNumbers.reduce((a, b) => a + b)
 }
