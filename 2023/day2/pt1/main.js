8420a30c35f9c887ccb152418d51e4a8e1b75d46
Ch3ru09 <xliang0905@gmail.com>
2023-12-02 21:51:14 -0500

    done day 2 part 1

---
+++ 2023/day2/pt1/main.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+let total = 0;
+const contained = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+// fs.readFile(path.join(__dirname, "./test-data.txt"), "utf-8", (_, data) => {
+fs.readFile(path.join(__dirname, "../data.txt"), "utf-8", (_, data) => {
+  data.split("\r\n").forEach((line, index) => {
+    for (let set of line.split(": ").at(-1).split("; ")) {
+      for (let color of set.split(", ")) {
+        color = color.split(" ");
+        if (color[0] > contained[color.at(-1)]) return;
+      }
+    }
+    total += index + 1;
+  });
+  console.log(total);
+});
+
+// 2217 too high
+// 2156
